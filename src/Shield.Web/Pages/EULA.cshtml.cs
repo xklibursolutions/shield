40e61832fd67f6c4408a1f3febcb4440a3ee28ba
Jonathan St-Michel <j.stmichel@xklibursolutions.io>
2024-10-23 11:45:07 -0400

    feat: add terms, eula and privacy document
    This is not yet translated.
    
    Close #10

---
+++ src/Shield.Web/Pages/EULA.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace XkliburSolutions.Shield.Web.Pages;
+
+/// <summary>
+/// Represents the model for the End User License Agreement (EULA) page.
+/// </summary>
+public class EULAModel : PageModel
+{
+    /// <summary>
+    /// Handles GET requests to display the EULA.
+    /// </summary>
+    public void OnGet()
+    {
+    }
+}
